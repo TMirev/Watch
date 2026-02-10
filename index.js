e5994ace5ff703debea973471191db7d2ca4d681
Tenko Mirev <t.d.mirev@gmail.com>
2026-02-10 09:51:03 +0000

    Add project files

---
+++ index.js
@@ -0,0 +1,68 @@
+const hourHand = document.querySelector(".hours");
+const minuteHand = document.querySelector(".minutes");
+const secondHand = document.querySelector(".seconds");
+
+function updateClock() {
+    const now = new Date();
+
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const secondsDeg = seconds * 6;
+    const minutesDeg = minutes * 6 + seconds * 0.1;
+    const hoursDeg = hours * 30 + minutes * 0.5;
+
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+const date = document.getElementById("dateDisplay");
+
+function updateDate() {
+    const now = new Date();
+
+    const day = now.getDate();
+    const month = now.getMonth() + 1;
+    const year = now.getFullYear();
+
+    let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    let today = new Date();
+    let dayNumber = today.getDay();
+    let dayName = weekDay[dayNumber];
+    document.getElementById("dayWeekDisplay").textContent = dayName;
+
+    const formatted = (`${day}/${month}/${year}`);
+
+    document.getElementById("dateDisplay").textContent = formatted;
+}
+
+
+setInterval(updateDate, 60000);
+updateDate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
